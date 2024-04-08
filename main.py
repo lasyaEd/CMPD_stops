73b4e166e19dda33edaa2d7cc7a865e8200db433
ledunuri <lasyaedunuri@gmail.com>
2024-04-08 19:04:37 -0400

    added stuff

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+
+@st.cache_data
+def load_data(csv):
+    df =pd.read_csv(csv)
+    return df
+stops = load_data("data/Officer_Traffic_Stops.csv")
+st.dataframe(stops)
+
